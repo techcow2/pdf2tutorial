b7541b747956d6abd3133ab58e3f36385220ffb5
techcow2 <techray@outlook.com>
2025-12-27 16:44:38 -0500

    fix chunk warnings

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'remotion': ['remotion', '@remotion/player'],
+          'pdfjs': ['pdfjs-dist'],
+        },
+      },
+    },
+  },
 })
